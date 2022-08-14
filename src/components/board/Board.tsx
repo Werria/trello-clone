ee9ef44475923d82f615bf74caf545c7098410d2
weria <verya.heydari@gmail.com>
2022-08-15 01:06:24 +0430

    adding list to context

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,6 +1,7 @@
 import React, {FC, useState} from "react"
 import {List} from "../../@types/List";
 import "./Board.css"
+import {addList, useBoardDispatch} from "../../contexts/BoardContext";
 
 interface IBoardLists{
     boardLists: List[];
@@ -10,12 +11,19 @@ export const Board: FC<IBoardLists> = ({boardLists}) => {
 
     const [listTitle, setListTitle] = useState("")
 
+    const boardDispatch = useBoardDispatch()
+
     const onListTitleChange = (listTitle: string) => {
         setListTitle(listTitle);
     }
 
     const onAddList = () => {
         if (listTitle.length) {
+            addList(boardDispatch, {
+                id: boardLists.length,
+                title: listTitle,
+                cards: []
+            })
             setListTitle("")
         }
     }
